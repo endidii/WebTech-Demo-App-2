2fa79ec921dec7ce8c67d8a80c2b30bc531b3ce3
endidii <thieuandyhuy@gmail.com>
2023-10-11 16:16:35 +0200

    erster commit

---
+++ src/test/java/de/htw/webtech2324endidi/gr2/Gr2ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htw.webtech2324endidi.gr2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gr2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
